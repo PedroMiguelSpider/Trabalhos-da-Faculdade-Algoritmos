6f6ee3aa2a16caa191b76527423562d0d1f1aec0
Pedro Miguel <60531301+PedroMiguelSpider@users.noreply.github.com>
2026-08-08 16:22:51 -0300

    Create Ex09.py

---
+++ Ex09.py
@@ -0,0 +1,17 @@
+def maior_numero(a, b):
+    if a > b:
+        return a
+    else:
+        return b
+
+while True:
+    num1 = float(input("Digite o primeiro número: "))
+    num2 = float(input("Digite o segundo número: "))
+    
+    if num != num:
+        break
+    else:
+        print("Os números são iguais! Digite novamente.\n")
+
+resultado = maior_numero(num1, num2)
+print(f"O maior número é: {resultado}")
