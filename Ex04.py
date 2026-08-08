ae9dd55923e571b096e2181b6a82c61e35609e18
Pedro Miguel <60531301+PedroMiguelSpider@users.noreply.github.com>
2026-08-08 16:03:03 -0300

    Ex04.py

---
+++ Ex04.py
@@ -0,0 +1,7 @@
+# Questão 4 - Leia a nota de um aluno e informe se ele foi aprovado (nota >= 7) ou reprovado.
+nota = float(input("Digite a nota do aluno: "))
+
+if nota >= 7:
+    print("Aluno aprovado!")
+else:
+    print("Aluno reprovado.")
