675975b3981c806fbea74ae11569264eb0d706a6
Pedro Miguel <60531301+PedroMiguelSpider@users.noreply.github.com>
2026-08-08 15:59:52 -0300

    Ex02.py

---
+++ Ex02.py
@@ -0,0 +1,5 @@
+# Questão 2 - Leia o nome e a idade de uma pessoa e exiba uma mensagem com essas informações.
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+
+print(f"Olá, {nome}! Você tem {idade} anos.")
