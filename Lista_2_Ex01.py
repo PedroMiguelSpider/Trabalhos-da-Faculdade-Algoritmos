dfac261af4dc274f90a54e6d09793c02fa007021
Pedro Miguel <60531301+PedroMiguelSpider@users.noreply.github.com>
2026-08-16 16:28:18 -0300

    Create Lista_2_Ex01.py

---
+++ Lista_2_Ex01.py
@@ -0,0 +1,8 @@
+# Questão 1. Criação e acesso
+# Crie uma lista chamada numeros contendo os valores 10, 20, 30, 40 e 50. Exiba o primeiro, o terceiro e o último elemento da lista. Em seguida, exiba a quantidade de elementos
+
+numeros = [10, 20, 30, 40, 50]
+print("Primeiro item:", numeros[0])
+print("Terceiro item:", numeros[2])
+print("Último item:", numeros[-1])
+print("Quantidade de itens na lista:", len(numeros))
